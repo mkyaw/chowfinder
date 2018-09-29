800433f9e5b4fe0496bd6053d9e073b4e982a818
Myo T Kyaw <myotkyaw@gmail.com>
2018-09-29 12:39:24 -0700

    Add presence validations to addresss and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  
   validates :name, presence: true
+  validates :address, presence:true
+  validates :description, presence:true
+  
   paginates_per 5
 end
